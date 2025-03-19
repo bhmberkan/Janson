67286f4a067f496b8813de3e2ddf72d5c85eb2f9
bhmberkan <berkanburakturgut@gmail.com>
2025-03-19 21:17:46 +0300

    Proje dosyası ekle.

---
+++ Janson/Controllers/MainController.cs
@@ -0,0 +1,49 @@
+﻿using Janson.Models;
+using Janson.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Janson.Controllers
+{
+    public class MainController : Controller
+    {
+        JonsonDbMvcEntities db = new JonsonDbMvcEntities(); 
+        // GET: Main
+        public ActionResult Index()
+        {
+            var values = db.MainTBL.ToList();
+            return View(values);
+        }
+
+        public PartialViewResult AboutUsPartial()
+        {
+            return PartialView();
+        }
+        public PartialViewResult AboutUsPartialprt()
+        {
+            return PartialView();
+        }
+        public PartialViewResult MainExperiencePartial()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult MainHobiPartial()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult MainGaleryPartial()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult ContactPartial()
+        {
+            return PartialView();
+        }
+    }
+}
