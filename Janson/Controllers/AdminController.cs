a0a6e811c20eba8c1d3c7d289c0cc0a3c1117a92
bhmberkan <berkanburakturgut@gmail.com>
2025-03-22 23:34:34 +0300

    Admin Laoutu aktardım. düzenledim. Admin index sayfasını yazdım.

---
+++ Janson/Controllers/AdminController.cs
@@ -0,0 +1,30 @@
+﻿using Janson.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Janson.Controllers
+{
+    public class AdminController : Controller
+    {
+        JonsonDbMvcEntities db = new JonsonDbMvcEntities();
+        // GET: Admin
+        public ActionResult Index()
+        {
+            var values = db.MainTBL.ToList();
+            return View(values);
+        }
+
+        public PartialViewResult SideBarPartial()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult AdminHeader()
+        {
+            return PartialView();
+        }
+    }
+}
