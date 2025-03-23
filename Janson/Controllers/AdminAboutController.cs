048852cba0cac098f86392d37f90f7bf073ed767
bhmberkan <berkanburakturgut@gmail.com>
2025-03-23 23:01:59 +0300

    Admin About Ve Admin About Partial kısımları yazıldı. SideBar güncellemeleri oldu. index update insert gibi sayfalar kodlandı.

---
+++ Janson/Controllers/AdminAboutController.cs
@@ -0,0 +1,43 @@
+﻿using Janson.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Janson.Controllers
+{
+    public class AdminAboutController : Controller
+    {
+      
+        JonsonDbMvcEntities db = new JonsonDbMvcEntities();
+        // GET: Admin
+        public ActionResult Index()
+        {
+            var values = db.AboutTBL.ToList();
+            return View(values);
+        }
+
+       
+
+        [HttpGet]
+        public ActionResult UpdateAboutPage(int id)
+        {
+            var values = db.AboutTBL.Find(id);
+            return View("UpdateAboutPage", values);
+        }
+        [HttpPost]
+        public ActionResult UpdateAboutPages(AboutTBL t)
+        {
+            var value = db.AboutTBL.Find(t.ID);
+            value.icerik = t.icerik;
+            value.icerik2 = t.icerik2;
+            value.photo = t.photo;
+           
+            db.SaveChanges();
+            return RedirectToAction("Index");
+
+
+        }
+    }
+}
---
+++ Janson/Controllers/AdminAboutPController.cs
@@ -0,0 +1,76 @@
+﻿using Janson.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Janson.Controllers
+{
+    public class AdminAboutPController : Controller
+    {
+        // GET: AdminAboutP
+        JonsonDbMvcEntities db = new JonsonDbMvcEntities();
+        // GET: Admin
+        public ActionResult Index()
+        {
+            var values = db.AboutpartTbl.ToList();
+            return View(values);
+        }
+
+
+        [HttpGet]
+        public ActionResult InsertAboutPPage()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult InsertAboutPPage(AboutpartTbl t)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("InsertAboutPPage");
+            }
+            db.AboutpartTbl.Add(t);
+            db.SaveChanges();
+            return RedirectToAction("InsertAboutPPage");
+
+        }
+
+
+        [HttpGet]
+        public ActionResult UpdateAboutPPage(int id)
+        {
+            var values = db.AboutpartTbl.Find(id);
+            return View("UpdateAboutPPage", values);
+        }
+
+        [HttpPost]
+        public ActionResult UpdateAboutPPages(AboutpartTbl t)
+        {
+            var value = db.AboutpartTbl.Find(t.ID);
+            value.Baslik = t.Baslik;
+            value.icerik = t.icerik;
+
+            db.SaveChanges();
+            return RedirectToAction("Index","AdminAbout");
+
+
+        }
+
+        public ActionResult DeleteAboutPPage(int id)
+        {
+            var prt = db.AboutpartTbl.Find(id);
+            db.AboutpartTbl.Remove(prt);
+            db.SaveChanges();
+            return RedirectToAction("Index","AdminAbout");
+        }
+
+        public PartialViewResult AdminAboutPPartial()
+        {
+            var values = db.AboutpartTbl.ToList();
+            return PartialView(values);
+        }
+    }
+}
