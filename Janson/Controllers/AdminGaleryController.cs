d7817f94f2c8027b6c2c021a3e8bc18207d45cea
bhmberkan <berkanburakturgut@gmail.com>
2025-03-24 23:26:39 +0300

    Admin Galery Ve Admin Hobies Controlleri yazıldı. Sidebar'a ekleme yapıldı, Galery(CRUD),Hobies(CRUD) işlemleri için sayfalar yazıldı.
    Eksikler;
    Mesajlar için admin tarafında page oluşturulacak.
    DowlandCv butonu kodlanacak
    Let'sTalk yerine login paneline yönlendirme yapılacak Login paneli Kodlanacak.
    Blog yazılmaya başlanacak

---
+++ Janson/Controllers/AdminGaleryController.cs
@@ -0,0 +1,69 @@
+﻿using Janson.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Janson.Controllers
+{
+    public class AdminGaleryController : Controller
+    {
+        JonsonDbMvcEntities db = new JonsonDbMvcEntities();
+        // GET: Admin
+        public ActionResult Index()
+        {
+            var values = db.PhotoTbl.ToList();
+            return View(values);
+        }
+
+
+        [HttpGet]
+        public ActionResult InsertGaleryPage()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult InsertGaleryPage(PhotoTbl t)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("InsertGaleryPage");
+            }
+            db.PhotoTbl.Add(t);
+            db.SaveChanges();
+            return RedirectToAction("InsertGaleryPage");
+
+        }
+
+
+        [HttpGet]
+        public ActionResult UpdateGaleryPage(int id)
+        {
+            var values = db.PhotoTbl.Find(id);
+            return View("UpdateGaleryPage", values);
+        }
+
+        [HttpPost]
+        public ActionResult UpdateGaleryPages(PhotoTbl t)
+        {
+            var value = db.PhotoTbl.Find(t.ID);
+            value.photo = t.photo;
+
+            db.SaveChanges();
+            return RedirectToAction("Index", "AdminGalery");
+
+
+        }
+
+        public ActionResult DeleteGaleryPage(int id)
+        {
+            var prt = db.PhotoTbl.Find(id);
+            db.PhotoTbl.Remove(prt);
+            db.SaveChanges();
+            return RedirectToAction("Index", "AdminGalery");
+        }
+
+    }
+}
---
+++ Janson/Controllers/AdminHobiesController.cs
@@ -0,0 +1,71 @@
+﻿using Janson.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Janson.Controllers
+{
+    public class AdminHobiesController : Controller
+    {
+        JonsonDbMvcEntities db = new JonsonDbMvcEntities();
+        // GET: Admin
+        public ActionResult Index()
+        {
+            var values = db.HobilerTBL.ToList();
+            return View(values);
+        }
+
+
+        [HttpGet]
+        public ActionResult InsertHobiesPage()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult InsertHobiesPage(HobilerTBL t)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("InsertHobiesPage");
+            }
+            db.HobilerTBL.Add(t);
+            db.SaveChanges();
+            return RedirectToAction("InsertHobiesPage");
+
+        }
+
+
+        [HttpGet]
+        public ActionResult UpdateHobiesPage(int id)
+        {
+            var values = db.HobilerTBL.Find(id);
+            return View("UpdateHobiesPage", values);
+        }
+
+        [HttpPost]
+        public ActionResult UpdateHobiesPages(HobilerTBL t)
+        {
+            var value = db.HobilerTBL.Find(t.ID);
+            value.Baslik = t.Baslik;
+            value.İcerik = t.İcerik;
+
+            db.SaveChanges();
+            return RedirectToAction("Index", "AdminHobies");
+
+
+        }
+
+        public ActionResult DeleteHobiesPage(int id)
+        {
+            var prt = db.HobilerTBL.Find(id);
+            db.HobilerTBL.Remove(prt);
+            db.SaveChanges();
+            return RedirectToAction("Index", "AdminHobies");
+        }
+
+        
+    }
+}
