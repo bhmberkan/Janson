4934608c4cbaf35b01e1114b34f4119a148de7c6
bhmberkan <berkanburakturgut@gmail.com>
2025-03-26 23:29:14 +0300

    Admin Mesajlar kısmını kodladım. About kısmının rengini istediğim gibi yaptım. Layoutta düzenlemeler yaptım. linkleri düzelttim.
    Eksikler:
    Login - Blog

---
+++ Janson/Controllers/AdminMessageController.cs
@@ -0,0 +1,34 @@
+﻿using Janson.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Janson.Controllers
+{
+    public class AdminMessageController : Controller
+    {
+        JonsonDbMvcEntities db = new JonsonDbMvcEntities();
+        // GET: AdminMessage
+        public ActionResult Index()
+        {
+            var values = db.MessageTbl.ToList();
+            return View(values);
+        }
+
+        public ActionResult MessageDetails(int id)
+        {
+            var value = db.MessageTbl.Find(id);
+            return View(value);
+        }
+
+        public ActionResult RemoveMessage(int id)
+        {
+            var value = db.MessageTbl.Find(id);
+            db.MessageTbl.Remove(value);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
