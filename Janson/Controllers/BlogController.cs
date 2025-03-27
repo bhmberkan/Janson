b7864bfb7bf292b6988e6db069d2c3cca3f45e0a
bhmberkan <berkanburakturgut@gmail.com>
2025-03-28 00:36:30 +0300

    Main tarafında Blog Sayfası için veri tabanıma eklemeler yaptım ve ilişkileri kurdum. Category işlemler için şu anlık ilişkili tablo kullanımına gitmedim. Blog Ve Blog deatils için de ayrı ayrı tablolar açmak istemedim. Blog mesajları için ayrıca bir tablo açıp ilişki kurdum. Daha sonra blog kısmının tasarımını ve kodlama işlemini gerçekleştirdim.
    eskikler.
    Paging işlemi,BlogDeatils Ve Bunların admin tarafında geçilmesi

---
+++ Janson/Controllers/BlogController.cs
@@ -0,0 +1,39 @@
+﻿using Janson.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Janson.Controllers
+{
+    public class BlogController : Controller
+    {
+        JonsonDbMvcEntities db = new JonsonDbMvcEntities();
+        // GET: Blog
+        public ActionResult Index()
+        {
+            /*
+            ViewBag.categoryCounts = db.BlogTBL
+                                    .GroupBy(x => x.Category)
+                                    .Select(g => new
+                                    {
+                                    Category = g.Key,
+                                    Count = g.Count()
+                                    }).ToList(); */
+
+            //Categorylerin sayısını çekmeyeceğım
+
+            var values = db.BlogTBL.ToList();
+            return View(values);
+
+        }
+
+        public PartialViewResult RecendPartial()
+        {
+            var values = db.BlogTBL.OrderByDescending(x=>x.ID).Take(4).ToList();
+            
+            return PartialView(values);
+        }
+    }
+}
